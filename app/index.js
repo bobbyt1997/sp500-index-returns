a99a402e5644a3190595301745e9522b7b451de7
Bobby Ton <bobbyt1997@gmail.com>
2020-11-05 15:29:19 -0600

    Modify index.js to render Main component

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './Main';
 
 ReactDOM.render(
-  <div>S&P Index Returns</div>,
+  <div>
+    <Main />
+  </div>,
   document.getElementById('app')
 );
