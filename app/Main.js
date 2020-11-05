9b6938bcd804d24740493e37a74ed56808ca7222
Bobby Ton <bobbyt1997@gmail.com>
2020-11-05 15:44:58 -0600

    Main component modified to display all information available within data.js

--- app/Main.js
+++ app/Main.js
@@ -10,8 +10,8 @@ export default class Main extends Component {
   }
 
   componentDidMount() {
-    axios.get('/api/').then((data) => {
-      this.setState({ returns: data });
+    axios.get('/api/').then((response) => {
+      this.setState({ returns: response.data });
     });
   }
 
@@ -20,7 +20,26 @@ export default class Main extends Component {
 
     return (
       <div>
-        MAIN COMPONENT
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Year</th>
+              <th scope="col">Total Return</th>
+              <th scope="col">Cumulative Return</th>
+            </tr>
+          </thead>
+          <tbody>
+            {returns ? returns.map((indexReturnObj, index) => {
+              return (
+                <tr key={index}>
+                  <th scope="row">{indexReturnObj.year}</th>
+                  <td>{indexReturnObj.totalReturn}</td>
+                  <td>CUMULATIVE RETURN</td>
+                </tr>
+              );
+            }) : <h1>Loading...</h1>}
+          </tbody>
+        </table>
       </div>
     )
   }
